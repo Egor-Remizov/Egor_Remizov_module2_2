f263825af4bcb80f4fb50815fd19afdc8fd9fcbd
Egor-Remizov <166824686+Egor-Remizov@users.noreply.github.com>
2024-12-28 16:02:46 +0300

    Delete ДР_услоная_конструкция_операторы.py

--- ДР_услоная_конструкция_операторы.py
+++
@@ -1,7 +0,0 @@
-first, second, third = int(input()), int(input()), int(input())
-if first == second == third:
-    print(3)
-elif first != second != third:
-    print(0)
-else first == second or second == third or first == third:
-    print(2)
